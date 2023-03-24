8515049dcea146bf1566612d25b9430d70d803e1
Raul F. De Souza <115673322+Ael1an@users.noreply.github.com>
2023-03-24 10:17:49 -0300

    Fundamentos da orientação a objetos

---
+++ bytebank/DesafioFixaxao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Desafio:  O cliente informou a necessidade de cadastrar informações sobre o nome, fabricante, código de barras, valor unitário,
+valor de compra e quantidade em estoque de determinado produto. */
+
+namespace bytebank
+{
+    public class ProdutoMercado
+    { 
+        public double valorUnitario;
+        public double valorDeCompra;
+        public double quantidadeEmEstoque;
+        public string codigoDeBarras;
+        public string nome;
+        public string fabricante;
+    }
+
+}
---
+++ bytebank/bytebank/Program.cs
@@ -0,0 +1,59 @@
+﻿using bytebank;
+
+//ContaCorrente contaAndre = new ContaCorrente();
+//contaAndre.titular = "André Silva";
+//contaAndre.numero_agencia = 15;
+//contaAndre.conta = "1010-x";
+//contaAndre.saldo = 100;
+
+//Console.WriteLine($"saldo do André: R${String.Format("{0:0.00}", contaAndre.saldo)}");
+
+
+///* Testes e execicios para aprender orientacão a objetos. 
+
+// contaAndre.depositar(100);
+
+//Console.WriteLine($"O saldo do André após deposito: R${String.Format("{0:0.00}", contaAndre.saldo)}");
+
+//if (contaAndre.sacar(300) == true)
+//{
+//    Console.WriteLine($"O saldo do André após saque: R${String.Format("{0:0.00}", contaAndre.saldo)}");
+
+//}
+//else
+//{
+//    Console.WriteLine("Saldo insuficiente para saque.");
+//} */
+
+//ContaCorrente contaMaria = new ContaCorrente();
+//contaMaria.titular = "Maria Souza";
+//contaMaria.numero_agencia = 17;
+//contaMaria.conta = "1010-5";
+//contaMaria.saldo = 350;
+
+
+
+//Console.WriteLine($"saldo da Maria: R${String.Format("{0:0.00}", contaMaria.saldo)}");
+
+//contaAndre.Transferir(50, contaMaria);
+
+//Console.WriteLine($"saldo do André: R${String.Format("{0:0.00}", contaAndre.saldo)}");
+//Console.WriteLine($"saldo da Maria: R${String.Format("{0:0.00}", contaMaria.saldo)}");
+//Console.WriteLine(string.Empty);
+
+//ContaCorrente contaPedro = new ContaCorrente();
+//contaPedro.titular = "Pedro silva";
+//Console.WriteLine(contaPedro.titular);
+//Console.WriteLine(contaPedro.numero_agencia);
+//Console.WriteLine(contaPedro.conta);
+//Console.WriteLine(contaPedro.saldo);
+
+Console.ReadKey();
+
+/* tipo por valor
+double valor = 300;
+double valor2 = valor;
+
+Console.WriteLine(valor==valor2);
+Console.WriteLine(valor);
+Console.WriteLine(valor2); */
